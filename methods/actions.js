f74bd992cedcfeb747d949e0b062d0fec591e33c
unknown <jerrycifeanyi@gmail.com>
2022-07-05 20:34:15 +0100

    change from REDIS_tsl_URL to REDIS_URL

--- methods/actions.js
+++ methods/actions.js
@@ -9,7 +9,7 @@ const generateOTP = require("../middleware/generateOTP");
 var AddressStore = require("../models/address");
 
 //for redis
-const client = createClient({ url: process.env.REDIS_TLS_URL });
+const client = createClient({ url: process.env.REDIS_URL });
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
