fc8130707f17bfa2c2697f18a7e177ebdde826e7
unknown <jerrycifeanyi@gmail.com>
2023-09-14 06:21:31 +0100

    upgraded the transaction section

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 export const formatAmount = (value) => {
     const truncate = value.toFixed(8)
-    const removeZeros = truncate.parseFloat();
+    const removeZeros = parseFloat(truncate);;
     return removeZeros;
 }
