e4731254afd10e1428c6194f8ca3e607b23fe8e1
jerrywebman <jerrycifeanyi@gmail.com>
2026-01-30 06:36:23 +0100

    added the tinqlab contact us feature

--- middleware/emailTemplate.js
+++ middleware/emailTemplate.js
@@ -15,6 +15,8 @@ var functions = {
       email: "info@tinqlab.com",
     };
 
+    //done
+
     const recievers = [{ email: email }];
     let htmlWelcomeTemplate = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
